9d7a57b022cd4deab43d5a3f88d2a5c66d2b9d4b
xuyang <xyy.hit@126.com>
2018-08-02 21:45:54 +0800

    1.fix

--- routes/bitforex.js
+++ routes/bitforex.js
@@ -216,7 +216,7 @@ function getMatchPrice(market, price_cb) {
       var min_sell = body.data.sell;
       var max_buy = body.data.buy;
       var sub = min_sell - max_buy;
-      if (sub > 0) {
+      if (sub > 0.000002) {
         var price = max_buy + parseFloat((sub / 2).toFixed(4));
         console.log(min_sell + " " + price + " " + max_buy);
         price_cb({
