5000428d0892574b4b80115c8bcb767768975cd5
xuyang <xyy.hit@126.com>
2018-08-07 15:54:04 +0800

    1.FIX

--- routes/coinall.js
+++ routes/coinall.js
@@ -4,6 +4,7 @@ var request = require('request');
 var signature = require('../utils/signature');
 var settings = require('../settings');
 var async = require('async');
+let API_URI = 'https://www.coinall.com';
 
 router.get('/account/wallet', (req, res, next) => {
   let currTime = Date.now() / 1000;
@@ -29,29 +30,35 @@ router.get('/account/wallet', (req, res, next) => {
     })
   })
 })
-
-function getLocalTime(i) {
-  //参数i为时区值数字，比如北京为东八区则输进8,西5输入-5
-  if (typeof i !== 'number') return;
-  var d = new Date();
-  //得到1970年一月一日到现在的秒数
-  var len = d.getTime();
-  //本地时间与GMT时间的时间偏移差
-  var offset = d.getTimezoneOffset() * 60000;
-  //得到现在的格林尼治时间
-  var utcTime = len + offset;
-  return utcTime + 3600000 * i;
-}
-
-function getServerTime(currTime) {
-  var options = {
-    url: 'https://www.coinall.com/api/general/v3/time',
-    method: 'GET',
-    json: true
+// 查询我的虚拟币信息
+router.get('/account/balance', (req, res, next) => {
+  let currTime = Date.now() / 1000;
+  let user = req.query.user;
+  var path = '/api/spot/v3/accounts';
+  if (req.query.currency) {
+    path += "/" + req.query.currency;
   }
-  request(options, (err, response, body) => {
-    currTime(body.epoch);
+  signature.coinall(currTime, 'GET', path, settings.coinall[user].secret_key, '', false, (cb) => {
+    console.log(JSON.stringify(cb));
+    var options = {
+      url: API_URI + path,
+      headers = {
+        'OK-ACCESS-KEY': settings.coinall[0].access_id,
+        'OK-ACCESS-SIGN': cb.signature,
+        'OK-ACCESS-TIMESTAMP': currTime,
+        'OK-ACCESS-PASSPHRASE': settings.coinall[0].Passphrase
+      },
+      method: 'GET',
+      json: true
+    }
+    request(options, (err, response, body) => {
+      if (err) {
+
+      } else {
+        res.json(body);
+      }
+    })
   })
-}
+})
 
 module.exports = router;
