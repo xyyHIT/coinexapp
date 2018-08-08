85c0d45fe3676d3f6ffc62ab3b794b4e97006ffb
xuyang <xyy.hit@126.com>
2018-08-08 20:03:10 +0800

    1.fix

--- routes/bigone.js
+++ routes/bigone.js
@@ -7,7 +7,6 @@ var async = require('async');
 let API_URI = 'https://big.one/api/v2';
 
 router.get('/viewer/accounts', (req, res, next) => {
-
   signature.bigone(settings.bigone[0].access_id, settings.bigone[0].secret_key, (cb) => {
     console.log(JSON.stringify(cb));
     let options = {
@@ -15,6 +14,87 @@ router.get('/viewer/accounts', (req, res, next) => {
       method: 'get',
       headers: {
         Authorization: "Bearer " + cb.signature
+      },
+      json: true
+    }
+    request(options, (err, response, body) => {
+      if (err) {
+
+      } else {
+        res.json(body);
+      }
+    })
+  })
+})
+
+router.get('/market/depth', (req, res, next) => {
+  let market = req.query.market;
+  signature.bigone(settings.bigone[0].access_id, settings.bigone[0].secret_key, (cb) => {
+    console.log(JSON.stringify(cb));
+    let options = {
+      url: API_URI + '/markets/' + market + '/depth',
+      method: 'get',
+      headers: {
+        Authorization: "Bearer " + cb.signature
+      },
+      json: true
+    }
+    request(options, (err, response, body) => {
+      if (err) {
+
+      } else {
+        res.json(body);
+      }
+    })
+  })
+})
+
+router.get('/market/limitOrder', (req, res, next) => {
+  let market = req.query.market;
+  let user = req.query.user;
+  let type = req.query.type;
+  let price = req.query.price;
+  let amount = req.query.amount;
+  signature.bigone(settings.bigone[user].access_id, settings.bigone[user].secret_key, (cb) => {
+    console.log(JSON.stringify(cb));
+    let options = {
+      url: API_URI + '/markets/' + market + '/depth',
+      method: 'post',
+      headers: {
+        Authorization: "Bearer " + cb.signature
+      },
+      json: true,
+      form: {
+        market_id: market,
+        side: type,
+        price: price,
+        amount: amount
+      }
+    }
+    request(options, (err, response, body) => {
+      if (err) {
+
+      } else {
+        res.json(body);
+      }
+    })
+  })
+})
+
+router.get('/market/cancelOrder', (req, res, next) => {
+  let user = req.query.user;
+  let order_id = req.query.orderId;
+  signature.bigone(settings.bigone[user].access_id, settings.bigone[user].secret_key, (cb) => {
+    console.log(JSON.stringify(cb));
+    let options = {
+      url: API_URI + '/viewer/orders/' + order_id + '/cancel',
+      method: 'post',
+      headers: {
+        Authorization: "Bearer " + cb.signature
+      },
+      json: true,
+      form: {
+        order_id: order_id
       }
     }
     request(options, (err, response, body) => {
